573d3d552c4334da91536aa475275ae12c6b0bca
Alex Alonso <alex.alonso-ext@viavarejo.com.br>
2020-07-08 23:16:00 -0300

    Reproduces issue

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+  color: ${({ color }) => color};
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  const computedColor = process.browser ? "green" : "red";
+  const [color, setColor] = useState(computedColor);
+
+  useEffect(() => {
+    setColor(computedColor);
+  }, []);
+
+  return <Title color={color}>I'm expecting to be {color}</Title>;
 }
